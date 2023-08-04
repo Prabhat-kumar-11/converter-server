38cc6ba286852ab84e0018a4837242f95a21c080
Prabhat Kumar <prabhssgg3@gmail.com>
2023-08-04 21:56:40 +0530

    ready server

--- index.js
+++ index.js
@@ -1,87 +1,51 @@
 const express = require('express');
-const { Configuration, OpenAIApi } = require("openai");
-const cors=require("cors")
-require("dotenv").config();
+const { Configuration, OpenAIApi } = require('openai');
+const cors = require('cors');
+require('dotenv').config();
+
 const app = express();
+const PORT = process.env.PORT || 3000;
 
-
-const configuration = new Configuration({
-  apiKey:process.env.OPENAI_API_KEY
-});
-app.use(cors())
-
-
-app.use(express.json())
-
-
-const openai = new OpenAIApi(configuration)
+app.use(cors());
 app.use(express.json());
 
+const openaiConfig = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
 
-app.get("/",(req,res)=>{
-  res.send("hello")
-})
+const openai = new OpenAIApi(openaiConfig);
 
+app.get('/', (req, res) => {
+  res.send('Hello');
+});
 
-app.post("/code/:data",async (req,res)=>{
-  let {data} = req.params
-  let {code}=req.body   
-  console.log(code,data)
+async function generateResponse(req, res, prompt) {
   try {
-   
-      const completion = await openai.createCompletion({
-        model:"text-davinci-003",
-        prompt:`Convert this code ${code} into ${data}`,
-        max_tokens: 2048,
-      });
-      res.send(completion.data.choices[0].text);
-    
+    const { code } = req.body;
+    const completion = await openai.createCompletion({
+      model: 'text-davinci-003',
+      prompt,
+      max_tokens: 2048,
+    });
+    res.send(completion.data.choices[0].text);
   } catch (error) {
-    res.send(
-      'error'
-    )
+    res.send('error');
   }
-})
+}
 
-app.post("/debug",async (req,res)=>{
+app.post('/code/:data', async (req, res) => {
+  const { data } = req.params;
+  await generateResponse(req, res, `Convert this code ${req.body.code} into ${data}`);
+});
 
-  let {code}=req.body   
-  console.log(code)
-  try {
-   
-      const completion = await openai.createCompletion({
-        model:"text-davinci-003",
-        prompt:`Debug this code ${code} if there is error in it `,
-        max_tokens: 2048,
-      });
-      res.send(completion.data.choices[0].text);
-    
-  } catch (error) {
-    res.send(
-      'error'
-    )
-  }
-})
+app.post('/debug', async (req, res) => {
+  await generateResponse(req, res, `Debug this code ${req.body.code} if there is an error in it`);
+});
 
-app.post("/quality",async (req,res)=>{
+app.post('/quality', async (req, res) => {
+  await generateResponse(req, res, `Check this code ${req.body.code} quality is good or not`);
+});
 
-  let {code}=req.body   
-  console.log(code)
-  try {
-   
-      const completion = await openai.createCompletion({
-        model:"text-davinci-003",
-        prompt:`Check  this code ${code} quality is good or not `,
-        max_tokens: 2048,
-      });
-      res.send(completion.data.choices[0].text);
-    
-  } catch (error) {
-    res.send(
-      'error'
-    )
-  }
-})
-app.listen(process.env.port, () => {
-  console.log(`Server listening on port ${process.env.port}`);
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
 });
